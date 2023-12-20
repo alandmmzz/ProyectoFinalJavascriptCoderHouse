665fda2c1739b796cf0135bf00cffddce80bb74b
Aland <alandmarp11@gmail.com>
2023-12-20 01:07:54 -0300

    Added subjects to JSON

--- js/javascript.js
+++ js/javascript.js
@@ -82,6 +82,27 @@ if (getTaskList == null) {
   UpdateLists();
 }
 
+
+let subjects = [];
+const subjectList = document.querySelector("ul#subjectList");
+const subjectSelector = document.querySelector("select#taskSubject");
+getSubjects();
+
+async function getSubjects() {
+  const response = await fetch("/js/subjects.json");
+  const subjectsJSON = await response.json();
+  subjects.push(...subjectsJSON)
+  subjects.forEach((subject)=> {
+  subjectList.innerHTML += `<li id="subject${subject.id}" class="list-group-item text-light d-flex justify-content-between align-items-center" style="background-color: ${subject.colour};">
+                            ${subject.name}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                </svg>
+                            </li>`;
+  subjectSelector.innerHTML += `<option value="${subject.name}">${subject.name}</option>`;
+  })
+}
+
 function openTaskInformation(id) {
   let showTask = taskList.find((task)=> task.id == id);
   console.log();
@@ -163,7 +184,6 @@ acceptBtn.addEventListener("click", () => {
     taskDescription
   );
   taskList.push(newTask);
-  console.log(taskList);
   UpdateLists();
   addTaskModal.style.display = "none";
   Toastify({
@@ -222,12 +242,4 @@ function UpdateLists(){
   taskList.length < 1 ?
     noTasksContainer.style.display = "block" :
     noTasksContainer.style.display = "none";
-}
-
-let subjects = [];
-
-async function getSubjects() {
-  const response = await fetch("/js/subjects.json");
-  const subjectsJSON = await response.json();
-  await subjects.push(...subjectsJSON)
 }
